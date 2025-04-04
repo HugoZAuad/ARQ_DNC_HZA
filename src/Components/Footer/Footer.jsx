de1e0faf05b29356939e723d822157abbbf12aa8
HugoZAuad <hugozeymer@hotmail.com>
2025-04-04 11:27:38 -0300

    Criação do footer, feito a estilização e criado os icones das redes e das linguagens

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,77 @@
+import { Link } from "react-router-dom";
+
+//assets
+import "./Footer.css";
+import Logo from "../../assets/DNC_logo.svg";
+import BrasilIcon from "../../assets/brasilIcon.svg";
+import UsaIcon from "../../assets/usaIcon.svg";
+import FacebookIcon from "../../assets/facebookIcon.svg";
+import TwitterIcon from "../../assets/twitterIcon.svg";
+import InstragramIcon from "../../assets/instagramIcon.svg";
+import LinkedinIcon from "../../assets/linkedinIcon.svg";
+
+function Footer() {
+  return (
+    <footer>
+      <div className="container">
+        <div className="d-flex jc-space-between">
+          <div className="footer-logo-col">
+            <img src={Logo} className="footer-logo" alt="DNC LOGO" />
+            <p className="grey-1-color">
+              A escola que prepara você para as profissões em alta no mercado de
+              trabalho.
+            </p>
+            <div className="d-flex social-links">
+              <a href="https://google.com" target="_blank">
+                <img src={FacebookIcon} alt="" />
+              </a>
+              <a href="https://google.com" target="_blank">
+                <img src={TwitterIcon} alt="" />
+              </a>
+              <a href="https://google.com" target="_blank">
+                <img src={LinkedinIcon} alt="" />
+              </a>
+              <a href="https://google.com" target="_blank">
+                <img src={InstragramIcon} alt="" />
+              </a>
+            </div>
+          </div>
+          <div className="d-flex">
+            <div className="footer-col">
+              <h3>Pages</h3>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/about">About</Link>
+                </li>
+                <li>
+                  <Link to="/projects">Projects</Link>
+                </li>
+                <li>
+                  <Link to="/contact">Contact</Link>
+                </li>
+              </ul>
+            </div>
+            <div className="footer-col">
+                <h3>Contact</h3>
+                <p className="grey-1-color"> R. Justino Cobra, 61 – Vila Ema | São José dos Campos – SP | CEP 12243-030 </p>
+                <p className="grey-1-color"> suporte@escoladnc.com.br </p>
+                <p className="grey-1-color"> (19) 99187-4342 </p>
+            </div>
+          </div>
+        </div>
+        <div className="d-flex jc-space-between footer-copy">
+            <p className="grey-1-color">Copyright © DNC - 2024</p>
+            <div className="langs-area d-flex">
+                <img src={BrasilIcon} height="29px" alt="Icone brasil" />
+                <img src={UsaIcon} height="29px" alt="Icone Usa" />
+            </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;

dec8023a07e6a8d64a9b1f036f44110207e0c86c
HugoZAuad <hugozeymer@hotmail.com>
2025-04-04 14:00:45 -0300

    Feito a parte principal da home, baixado já os assets para o proximo passo

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,14 +1,19 @@
 import Banner from "../Components/Banner/Banner";
 import Button from "../Components/Button/Button";
 import Header from "../Components/Header/Header";
+import Hero from "../Components/Hero/Hero";
 import Footer from "../Components/Footer/Footer";
 
 function Home() {
   return (
     <>
       <Header />
-      <Button buttonStyle="secondary" arrow>olá!</Button>
-      <Banner title="Home" image="about.jpg" />
+      <Button buttonStyle="secondary" arrow>
+        olá!
+      </Button>
+      <div className="container">
+        <Hero />
+      </div>
       <Footer />
     </>
   );

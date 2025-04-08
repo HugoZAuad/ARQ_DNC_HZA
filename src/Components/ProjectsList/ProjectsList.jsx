8816577001a426fdd8f21a5ca7ec3a0180b8c4b4
HugoZAuad <hugozeymer@hotmail.com>
2025-04-08 14:35:21 -0300

    Feito a inclusão da apiREST no projeto, e alterado para pegar as informações da api para a aplicação.

--- src/Components/ProjectsList/ProjectsList.jsx
+++ src/Components/ProjectsList/ProjectsList.jsx
@@ -1,74 +1,54 @@
-import "./ProjectsList.css"
+import { useState, useEffect } from "react";
+import "./ProjectsList.css";
 
 //assets
-import LikedFilled from "../../assets/like-filed.svg"
-import Like from "../../assets/like.svg"
+import LikedFilled from "../../assets/like-filed.svg";
+import Like from "../../assets/like.svg";
+
+//Utils
+import { getApiData } from "../../services/apiservices";
 
 function ProjectsList() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const projectsResponse = await getApiData("projects");
+        setProjects(projectsResponse);
+      } catch {
+        setProjects([]);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="projects-section">
-        <div className="projects-hero"> 
-            <h2>Follow Our Projects</h2>
-            <p>It is a long established fact that a reader will be distracted by the of readable content of page  lookings at its layouts  points.</p>
-        </div>
-        <div className="projects-grid">
-            <div className="project-card d-flex jc-center al-center fd-column">
-                <div className="thumb tertiary-background">
-                    <h3>João Silva</h3>
-                    <p>BH, Brasil</p>
-                    <img src={Like} height="20px" alt="" />
-                </div>
-            </div>
-            <div className="project-card d-flex jc-center al-center fd-column">
-                <div className="thumb tertiary-background">
-                    <h3>João Silva</h3>
-                    <p>BH, Brasil</p>
-                    <img src={LikedFilled} height="20px" alt="" />
-                </div>
-            </div>
-            <div className="project-card d-flex jc-center al-center fd-column">
-                <div className="thumb tertiary-background">
-                    <h3>João Silva</h3>
-                    <p>BH, Brasil</p>
-                    <img src={LikedFilled} height="20px" alt="" />
-                </div>
-            </div>
-            <div className="project-card d-flex jc-center al-center fd-column">
-                <div className="thumb tertiary-background">
-                    <h3>João Silva</h3>
-                    <p>BH, Brasil</p>
-                    <img src={LikedFilled} height="20px" alt="" />
-                </div>
-            </div>
-            <div className="project-card d-flex jc-center al-center fd-column">
-                <div className="thumb tertiary-background">
-                    <h3>João Silva</h3>
-                    <p>BH, Brasil</p>
-                    <img src={LikedFilled} height="20px" alt="" />
-                </div>
-            </div>
-            <div className="project-card d-flex jc-center al-center fd-column">
-                <div className="thumb tertiary-background">
-                    <h3>João Silva</h3>
-                    <p>BH, Brasil</p>
-                    <img src={LikedFilled} height="20px" alt="" />
-                </div>
-            </div>
-            <div className="project-card d-flex jc-center al-center fd-column">
-                <div className="thumb tertiary-background">
-                    <h3>João Silva</h3>
-                    <p>BH, Brasil</p>
-                    <img src={LikedFilled} height="20px" alt="" />
-                </div>
-            </div>
-            <div className="project-card d-flex jc-center al-center fd-column">
-                <div className="thumb tertiary-background">
-                    <h3>João Silva</h3>
-                    <p>BH, Brasil</p>
-                    <img src={LikedFilled} height="20px" alt="" />
-                </div>
-            </div>
-        </div>
+      <div className="projects-hero">
+        <h2>Follow Our Projects</h2>
+        <p>
+          It is a long established fact that a reader will be distracted by the
+          of readable content of page lookings at its layouts points.
+        </p>
+      </div>
+      <div className="projects-grid">
+        {projects.map((project) => (
+          <div
+            className="project-card d-flex jc-center al-center fd-column"
+            key={project.id}
+          >
+            <div
+              className="thumb tertiary-background"
+              style={{ backgroundImage: `url(${project.thumb})` }}
+            ></div>
+            <h3>{project.title}</h3>
+            <p>{project.subtitle}</p>
+            <img src={LikedFilled} height="20px" alt="" />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }

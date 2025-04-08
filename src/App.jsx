36aca638476e36696a629945f88a29b28c644b5a
HugoZAuad <hugozeymer@hotmail.com>
2025-04-07 21:57:17 -0300

    Inserido o useEffect para o ciclo de vida da aplicação girar, e para melhorar a funcionalidade do menu no footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 /* PAGES */
-
 import Home from './Pages/Home'
 import About from './Pages/About'
 import Projects from './Pages/Projects'
 import Contact from './Pages/Contact'
+
+//Utils
+import ScrollToTop from './Utils/ScrollTop'
 
 
 function App() {
 
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
